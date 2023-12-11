e936b6ee8e4f944b59cf78e06a84e7fc20d04abe
raybagas7 <raysam.gas@gmail.com>
2023-12-12 04:33:29 +0700

    add cors

--- api/server.js
+++ api/server.js
@@ -1,14 +1,15 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require("json-server");
-const auth = require("json-server-auth");
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+const cors = require('cors');
 
 const server = jsonServer.create();
 
 // Uncomment to allow write operations
-const fs = require("fs");
-const path = require("path");
-const filePath = path.join("db.json");
-const data = fs.readFileSync(filePath, "utf-8");
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join('db.json');
+const data = fs.readFileSync(filePath, 'utf-8');
 const db = JSON.parse(data);
 const router = jsonServer.router(db);
 
@@ -18,11 +19,12 @@ const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 server.db = router.db;
 
+server.use(cors({ origin: '*', optionsSuccessStatus: 200 }));
 server.use(middlewares);
 server.use(auth);
 server.use(router);
 server.listen(3000, () => {
-  console.log("JSON Server is running");
+  console.log('JSON Server is running');
 });
 
 // Export the Server API
